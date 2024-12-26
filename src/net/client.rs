8043be2bb32d2fc67916e8d0ad3639119318365a
Kowkodivka <theeugenet@gmail.com>
2024-12-26 13:05:08 +0700

    Client + Events

---
+++ src/net/client.rs
@@ -0,0 +1,31 @@
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
+
+use super::models::Event;
+
+pub struct Client {
+    stream: TcpStream,
+}
+
+impl Client {
+    pub async fn send_event(&mut self, event: &Event) {
+        let serialized = bincode::serialize(event).unwrap();
+        self.stream.write_all(&serialized).await.unwrap();
+    }
+
+    pub async fn receive_event(&mut self) -> Option<Event> {
+        let mut buffer = [0; 1024];
+
+        let size = self.stream.read(&mut buffer).await.unwrap_or(0);
+
+        if size == 0 {
+            return None;
+        }
+
+        let event: Event = bincode::deserialize(&buffer[..size]).unwrap();
+
+        Some(event)
+    }
+}
---
+++ src/net/models.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Event {
+    opcode: u8,
+    data: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct HelloEvent {}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct HeartbeatEvent {}
+
+#[derive(Debug)]
+pub enum EventType {
+    Hello(HelloEvent),
+    Heartbeat(HeartbeatEvent),
+}
+
+pub fn deserialize(event: &Event) -> Option<EventType> {
+    match event.opcode {
+        0 => serde_json::from_str::<HelloEvent>(&event.data)
+            .ok()
+            .map(EventType::Hello),
+        1 => serde_json::from_str::<HeartbeatEvent>(&event.data)
+            .ok()
+            .map(EventType::Heartbeat),
+        _ => None,
+    }
+}
